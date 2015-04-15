131327cf657b511e9c681e0ab99d76b8ab752b73
Steve Suehs <skelter.net@gmail.com>
2015-04-14 22:29:37 -0500

    Try out different docs, upgrade cheatsheet

--- project.clj
+++ project.clj
@@ -6,5 +6,6 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [http-kit "2.1.18"]]
-  :plugins [[lein-instant-cheatsheet "2.1.3"]
-            [lein-marginalia "0.8.0"]])
+  :plugins [[lein-instant-cheatsheet "2.1.4"]
+            [lein-marginalia "0.8.0"]
+            [codox "0.8.11"]])
